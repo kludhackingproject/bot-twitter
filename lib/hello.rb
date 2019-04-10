bbb1c7b8bd7b8b6cf5e5d8a787e898bfba6e311d
kludDesign <vincentgillibert@gmail.com>
2019-04-10 14:06:50 +0200

    add twitter connection

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,17 @@
 require 'pry'
 require 'dotenv'
+require 'twitter'
 
 # to call your API keys
-Dotenv.load('../.env')
+Dotenv.load('.env')
 
-# to get your API key in a varible
-my_key = ENV['TWITTER_API_SECRET']
+  # quelques lignes qui appellent les clés d'API de ton fichier .env
+  client = Twitter::REST::Client.new do |config|
+   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
 
-def hello
-  return "Hello!"
-end
+# ligne qui permet de tweeter sur ton compte
+client.update("Hello world, mon premier tweet en Ruby via l'API twitter !")
--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/hello'
 
-describe "the hello function" do
-  it "says hello" do
-    expect(hello).to eq("Hello!")
+describe "the login_twitter method" do
+  it "should return client, and client is not nil" do
+    expect(login_twitter).not_to be_nil
   end
 end
